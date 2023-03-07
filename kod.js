4a4d2387bcd4a19984f34bbc07c689a531e7b751
Sonja Milic <sonjamilic996@gmail.com>
2023-03-07 18:13:19 +0100

    Domaći 2

---
+++ kod.js
@@ -0,0 +1,60 @@
+function myFunction() {
+   var broj = prompt("Insert number between 1 and 999");
+   var x = (broj)
+   if (x > 0 && x <= 999) {
+      var sqrt =  Math.sqrt(x);
+   alert(sqrt)
+   }
+   if (isNaN(x)) {
+      alert("This is not a number!")
+   }
+   if (x>999) {
+      alert("Number must be between 1 i 999!");
+   }
+}
+
+function myFunction1() {
+   var broj = prompt("Insert number between 1 and 999");
+   var x = (broj)
+   if (x > 0 && x <= 999) {
+      var sin =  Math.sin(x);
+   alert(sin)
+   }
+   if (isNaN(x)) {
+      alert("This is not a number!")
+   }
+   if (x>999) {
+      alert("Number must be between 1 i 999!");
+   }
+}
+
+function myFunction2() {
+   var broj = prompt("Insert number between 1 and 999");
+   var x = (broj)
+   if (x > 0 && x <= 999) {
+      var cos =  Math.cos(x);
+   alert(cos)
+   }
+   if (isNaN(x)) {
+      alert("This is not a number!")
+   }
+   if (x>999) {
+      alert("Number must be between 1 i 999!");
+   }
+}
+
+
+function myFunction3() {
+   var broj = prompt("Insert number between 1 and 999");
+   var x = (broj)
+   if (x > 0 && x <= 999) {
+      var round =  Math.round (x);
+   alert(round)
+   }
+   if (isNaN(x)) {
+      alert("This is not a number!")
+   }
+   if (x>999) {
+      alert("Number must be between 1 i 999!");
+   }
+}
